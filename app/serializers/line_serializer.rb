236b534fe2d0fae380f87566a1f0a64da05d26fc
Ben Yellin <byellindc@gmail.com>
2019-04-04 17:50:30 -0400

    add document_id to line serialization

--- app/serializers/line_serializer.rb
+++ app/serializers/line_serializer.rb
@@ -1,4 +1,4 @@
 class LineSerializer < ActiveModel::Serializer
-  attributes :id, :input, :processed, :name, :result
+  attributes :id, :input, :processed, :name, :result, :document_id
   belongs_to :document
 end
