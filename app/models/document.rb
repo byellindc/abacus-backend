e710a1aa8ab4a337c944f726b83c5c9a55a093db
Ben Yellin <byellindc@gmail.com>
2019-04-04 13:09:01 -0400

    add support for default title

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,9 @@ class Document < ApplicationRecord
   belongs_to :user
   has_many :lines, dependent: :destroy
 
+  DEFAULT_TITLE = 'Untitled'
+  after_create :ensure_title
+
   def eval(expression)
     calculator.eval(expression)
   end
@@ -15,6 +18,32 @@ class Document < ApplicationRecord
     # e.g. 1.0 => 1, 1.1 => 1.1
     total_val = raw_total
     total_val == total_val.to_i ? total_val.to_i : total_val.to_f
+  end
+
+  def ensure_title
+    # if no title is specified, generate incremented default
+    if !self.title
+      self.update(title: Document.next_default_title)
+    end
+  end
+
+  def self.last_default_title
+    Document
+      .where("title LIKE :prefix", prefix: "#{DEFAULT_TITLE}%")
+      .pluck(:title).last || DEFAULT_TITLE
+  end
+
+  def self.last_default_index
+    index = Document.last_default_title.scan(/\d+/).first.to_i
+    index == 0 ? 1 : index
+  end
+
+  def self.next_default_index
+    Document.last_default_index + 1
+  end
+
+  def self.next_default_title
+    "#{DEFAULT_TITLE} #{Document.next_default_index}"
   end
 
   private 
