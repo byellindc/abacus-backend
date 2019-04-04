c710f0634baf395576a3586076a09a6f2cac3cfa
Ben Yellin <byellindc@gmail.com>
2019-04-04 18:12:33 -0400

    switch line controller callbacks to memoization

--- app/controllers/api/v1/lines_controller.rb
+++ app/controllers/api/v1/lines_controller.rb
@@ -1,21 +1,18 @@
 class Api::V1::LinesController < ApplicationController
-  before_action :get_document
-  before_action :get_line, except: %i(index)
-
   def index
-    render json: @document.lines, status: :accepted
+    render json: document.lines, status: :accepted
   end
   
   def show
-    render json: @line, status: :accepted
+    render json: line, status: :accepted
   end
 
   def update
-    @line.update(line_params)
-    if @line.save
-      render json: @line, status: :accepted
+    line.update(line_params)
+    if line.save
+      render json: line, status: :accepted
     else
-      render json: { errors: @line.errors.full_messages }, status: :unprocessible_entity
+      render json: { errors: line.errors.full_messages }, status: :unprocessible_entity
     end
   end
   
@@ -25,11 +22,11 @@ class Api::V1::LinesController < ApplicationController
     params.permit(:input)
   end
 
-  def get_document
-    @document = Document.find(params[:document_id])
+  def document
+    @document ||= Document.find(params[:document_id])
   end
 
-  def get_line
-    @line = @document.find(params[:id])
+  def line
+    @line ||= document.lines.find(params[:id])
   end
 end
