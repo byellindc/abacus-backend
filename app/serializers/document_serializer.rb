9045615613b42ea64304f1510b845d8e19dac9b3
Ben Yellin <byellindc@gmail.com>
2019-04-04 12:33:12 -0400

    .

--- app/serializers/document_serializer.rb
+++ app/serializers/document_serializer.rb
@@ -1,4 +1,4 @@
 class DocumentSerializer < ActiveModel::Serializer
-  attributes :id, :title, :created_at, :updated_at
+  attributes :id, :title, :total, :created_at, :updated_at
   has_many :lines
 end
