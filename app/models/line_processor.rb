4f00f6418bdc002571cb277419a4d103ca53f588
Ben Yellin <byellindc@gmail.com>
2019-04-08 14:53:47 -0400

    implement process_percentage_expression

--- app/models/line_processor.rb
+++ app/models/line_processor.rb
@@ -34,7 +34,28 @@ class LineProcessor
     end
   end
 
+  # matches `[PERCENTAGE] of [NUM]`
+  # and `[PERCENTAGE] (off|on) [NUM]`
   def process_percentage_expression
+    regex = /(?<percentage>-?[\d.]+)% (?<operator>(off?|on)) (?<num>-?[\d.]+)/
+    match = @expression.match(regex)
+
+    if match
+      operator = match.named_captures["operator"]
+      percentage = match.named_captures["percentage"]
+      num = match.named_captures["num"]
+
+      percentage_expr = "#{num} * #{percentage.to_f/100}"
+
+      case operator
+      when 'of'
+        @expression = percentage_expr
+      when 'off'
+        @expression = "#{num} - (#{percentage_expr})"
+      when 'on'
+        @expression = "#{num} + (#{percentage_expr})"
+      end
+    end
   end
 
   def process_word_operators
