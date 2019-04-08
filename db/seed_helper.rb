1b9c38570c920d62b577e89b11eacd0c408593ea
Ben Yellin <byellindc@gmail.com>
2019-04-08 15:51:07 -0400

    improve random math seeding

--- db/seed_helper.rb
+++ db/seed_helper.rb
@@ -6,10 +6,22 @@ def rand_operator
   OPERATORS.sample
 end
 
+# random number between min/max
+# with occassional chance of being a float
+def rand_num(min = 1, max = 500)
+  if rand(0..4) == 0
+    rand(min.to_f..max.to_f).round(2)
+  else
+    rand(min..max)
+  end
+end
+
 def rand_math_expression(min: 1, max: 500, wrap: false)
-  tokens = [rand(min..max), rand_operator, rand(min..max)]
+  tokens = [rand_num(min, max), rand_operator, rand_num(min, max)]
+
   expr = tokens.join(' ')
   expr = "(#{expr})" if wrap
+
   return expr
 end
 
@@ -18,8 +30,8 @@ def rand_math_expressions
   len = rand(1..3)
 
   len.times do |i|
-    expr = rand_math_expr
-    wrap = len > 1 && rand(0..2) == 0
+    expr = rand_math_expression
+    wrap = len > 1 && (rand(0..2) == 0)
 
     if i == (len-1)
       expressions << rand_math_expression(wrap: wrap)
