b4a09b63a7ed2669defb11d41ce785c4b2d3fc23
Ben Yellin <byellindc@gmail.com>
2019-04-08 12:43:31 -0400

    cleanup Document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -28,28 +28,28 @@ class Document < ApplicationRecord
     lines.inject(0) {|total, l| total + l.result}
   end
 
+  # recast total if it can be expressed as an integer
+  # e.g. 1.0 => 1, 1.1 => 1.1
   def total
-    # recast total if it can be expressed as an integer
-    # e.g. 1.0 => 1, 1.1 => 1.1
     total_val = raw_total
     total_val == total_val.to_i ? total_val.to_i : total_val.to_f
   end
 
+  # if no title is specified, generate incremented default
   def ensure_title
-    # if no title is specified, generate incremented default
     self.update(title: Document.next_default_title) if !self.title
   end
 
+  # returns last title of default-titled document ('Untitled...')
   def self.last_default_title
-    # returns last title of default-titled document ('Untitled...')
     Document
       .where("title LIKE :prefix", prefix: "#{DEFAULT_TITLE} %")
       .pluck(:title).last || DEFAULT_TITLE
   end
 
+  # returns last numeric value of default-titled document
+  # defaults to 1
   def self.last_default_index
-    # returns last numeric value of default-titled document
-    # defaults to 1
     index = Document.last_default_title.scan(/\d+/).first.to_i
     index == 0 ? 1 : index
   end
@@ -62,10 +62,9 @@ class Document < ApplicationRecord
     "#{DEFAULT_TITLE} #{Document.next_default_index}"
   end
 
+  private 
+
   def calculator
     @calculator ||= Calculator.new
   end
-
-  private 
-
 end
