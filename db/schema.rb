75ca3db1427f11192cc22cc0982783e74d61ec78
Ben Yellin <byellindc@gmail.com>
2019-04-13 19:06:59 -0400

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_165232) do
+ActiveRecord::Schema.define(version: 2019_04_13_225842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_04_08_165232) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "mode"
+    t.integer "index"
     t.index ["document_id"], name: "index_lines_on_document_id"
   end
 
