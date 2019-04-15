84f8c8435cba0c4c3fef2904d4660dcd792d25d8
Ben Yellin <byellindc@gmail.com>
2019-04-14 23:22:45 -0400

    .

--- app/serializers/line_serializer.rb
+++
@@ -1,6 +0,0 @@
-class LineSerializer < ActiveModel::Serializer
-  attributes :id, :document_id,
-    :mode, :index, :input, :expression, 
-    :name, :result, :created_at, :updated_at
-  belongs_to :document
-end
