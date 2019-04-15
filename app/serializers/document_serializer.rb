385ff97378ede883a0da58391576476c33ebc4ea
Ben Yellin <byellindc@gmail.com>
2019-04-14 20:41:29 -0400

    serialize content attr

--- app/serializers/document_serializer.rb
+++ app/serializers/document_serializer.rb
@@ -1,6 +1,5 @@
 class DocumentSerializer < ActiveModel::Serializer
-  attributes :id, :title, 
-    :total, :raw_total,
-    :created_at, :updated_at
+  attributes :id, :title, :content, :total, 
+    :raw_total, :created_at, :updated_at
   has_many :lines
 end
