61e77f4f2b0f910b282416f05359fea36104908f
Ben Yellin <byellindc@gmail.com>
2019-04-14 20:40:35 -0400

    add numeric helper

---
+++ app/helpers/numeric_helper.rb
@@ -0,0 +1,15 @@
+class NumericHelper
+  def simplify_number(num)
+    if !num.is_a? Numeric
+      return num
+    elsif num == num.to_i
+      return num.to_i
+    else
+      return num.to_f
+    end
+  end
+
+  def strip_non_numeric(input)
+    input.gsub(/[^[:digit:]]/, '')
+  end
+end
